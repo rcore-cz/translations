1f366cefb504c832e6ff892e69055c1b7feafb42
Isigar <stanislav.opletal@gmail.com>
2023-05-05 11:48:13 +0200

    init translation

---
+++ rcore_clothes/cs.lua
@@ -0,0 +1,11 @@
+Locales['cs'] = {
+	['save_success'] = 'Vaše oblečení bylo uspěšně uloženo!',
+	['save_success_title'] = 'Uloženo!',
+	['diff_name'] = 'Zvolte jiný název pro vaše oblečení.',
+	['no_money'] = 'Na toto oblečení nemáte dostatek peněz, zkuste zvolit jinou platební metodu.',
+	['thx_buy'] = 'Děkujeme za nákup!',
+	['success_buy'] = 'Uspěšně jste si koupili nové oblečení! Děkujeme Vám za nákup a přejeme příjemný den!',
+	['delete_success'] = 'Uspěšně si vyhodil starý outfit!',
+	['delete_outfit'] = 'Vyhodit outfit',
+	['dress_up'] = 'Obléct se',
+}
---
+++ rcore_clothes/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+	['save_success'] = 'Your clothes have been saved successfully!',
+	['save_success_title'] = 'Saved!',
+	['diff_name'] = 'Choose another name for your clothes.',
+	['no_money'] = 'You don\'t have enough money for this outfit, try another payment method',
+	['thx_buy'] = 'Thank you for your purchase!',
+	['success_buy'] = 'You have successfully bought new clothes! Thank you for your purchase and have a nice day!',
+	['delete_success'] = 'You have successfully tossed the old outfit!',
+	['delete_outfit'] = 'Delete outfit',
+	['dress_up'] = 'Dress up',
+}
